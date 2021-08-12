ea906477e6c6d5351023381a3784d5846950a78d
Damienhb <snowdarkwinter@gmail.com>
2021-08-12 15:20:12 +0200

    merge

--- projetDoctolib-spring-data-jpa/src/main/java/repository/IAdresseRepository.java
+++ projetDoctolib-spring-data-jpa/src/main/java/repository/IAdresseRepository.java
@@ -1,5 +1,9 @@
 package repository;
 
-public interface IAdresseRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import model.Adresse;
+
+public interface IAdresseRepository extends JpaRepository<Adresse, Long> {
 
 }
