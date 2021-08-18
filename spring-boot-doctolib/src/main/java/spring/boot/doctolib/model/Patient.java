17a6c33207e62a94ff16f68188f6ee760b710b23
Aurélien <aurelien.dispot@gmail.com>
2021-08-18 17:21:08 +0200

    commit boot aurelien

--- spring-boot-doctolib/src/main/java/spring/boot/doctolib/model/Patient.java
+++ spring-boot-doctolib/src/main/java/spring/boot/doctolib/model/Patient.java
@@ -8,12 +8,16 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonView;
+
 @Entity
 @DiscriminatorValue("patient")
 public class Patient extends Personne {
 	@Column(name="carteVitale")
+	@JsonView(Views.ViewCommon.class)
 	private Integer carteVitale;
 	@OneToMany(mappedBy = "patient")
+	@JsonView(Views.ViewPatient.class)
 	private List<RendezVous> rendezVous = new ArrayList<RendezVous>();
 	
 	public Patient() {
---
+++ spring-boot-doctolib/src/main/java/spring/boot/doctolib/model/Views.java
@@ -0,0 +1,13 @@
+package spring.boot.doctolib.model;
+
+public class Views {
+
+	public static class ViewCommon {}
+
+	public static class ViewPersonne extends ViewCommon {}
+	
+	public static class ViewPatient extends ViewPersonne {}
+	
+	public static class ViewAdmin extends ViewPersonne {}
+
+}
