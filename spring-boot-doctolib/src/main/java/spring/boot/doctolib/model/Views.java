e0277d6aa8011ff79f186073c51bb406987ee4dd
Aurélien <aurelien.dispot@gmail.com>
2021-08-19 09:36:31 +0200

    commit change aurelien

--- spring-boot-doctolib/src/main/java/spring/boot/doctolib/model/Views.java
+++ spring-boot-doctolib/src/main/java/spring/boot/doctolib/model/Views.java
@@ -9,6 +9,7 @@ public class Views {
 	public static class ViewPatient extends ViewPersonne {}
 	
 	public static class ViewAdmin extends ViewPersonne {}
+	
 	public static class ViewRendezVous extends ViewCommon{}
 	
 	public static class ViewMotifConsultation extends ViewCommon{}
@@ -18,8 +19,9 @@ public class Views {
 	
 	public static class ViewPraticienSpecialite extends ViewCommon{}
 
-}
-
 	public static class ViewCreneauxHoraires extends ViewCommon {}
+	
 	public static class ViewAdresse extends ViewCommon {}
+	
 	public static class ViewLieux extends ViewCommon {}
+}
