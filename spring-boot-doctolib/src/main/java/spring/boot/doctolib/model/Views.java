5b733faeff3f4da6b43fdde3f1cde22e3065b062
Damienhb <snowdarkwinter@gmail.com>
2021-08-19 09:37:36 +0200

    Revert "Merge branch 'main' into damienHb"
    This reverts commit 6248ad7b9709ae03c81eabcbed1b54507747b1f3.

--- spring-boot-doctolib/src/main/java/spring/boot/doctolib/model/Views.java
+++ spring-boot-doctolib/src/main/java/spring/boot/doctolib/model/Views.java
@@ -9,21 +9,17 @@ public class Views {
 	public static class ViewPatient extends ViewPersonne {}
 	
 	public static class ViewAdmin extends ViewPersonne {}
-	
 	public static class ViewRendezVous extends ViewCommon{}
 	
 	public static class ViewMotifConsultation extends ViewCommon{}
 	
 	public static class ViewPraticien extends ViewPersonne{}
-	
 	public static class ViewSpecialite extends ViewCommon{}
 	
 	public static class ViewPraticienSpecialite extends ViewCommon{}
 
-	public static class ViewCreneauxHoraires extends ViewCommon {}
-	
-	public static class ViewAdresse extends ViewCommon {}
-	
-	public static class ViewLieux extends ViewCommon {}
-	
 }
+
+	public static class ViewCreneauxHoraires extends ViewCommon {}
+	public static class ViewAdresse extends ViewCommon {}
+	public static class ViewLieux extends ViewCommon {}
